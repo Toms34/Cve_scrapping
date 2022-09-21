8b4c0f353a52c8a31cb93bce3add9e3fc0a8a003
Thomas Bergon <39386540+Toms34@users.noreply.github.com>
2022-09-21 15:34:34 +0200

    bug fix

--- CveVxworks/spiders/Cve.py
+++ CveVxworks/spiders/Cve.py
@@ -1,23 +1,47 @@
-import scrapy , json 
+import scrapy , json , re
+from pydispatch import dispatcher
+from scrapy import signals
 
 class CveSpider(scrapy.Spider):
     name = 'Cve' #name of spider
     allowed_domains = ['cvedetails.com'] #allowed domains
+    cves = []
+    scores = []
+    final = []
     start_urls= ["https://www.cvedetails.com/vulnerability-list/vendor_id-95/product_id-15063/Windriver-Vxworks.html"] #url for cve details of vxworks 
 
     def __init__(self):
-        self.result=[]
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
 
     def parse(self, response):
-        cves = response.xpath('string(//body)').re(r"CVE-\d{4}-\d{4,7}") #get all CVEs regex CVE-dddd-(dddd ddd) <--- 4-7 digits
-        temp = response.xpath('//body//*//td//div//text()').getall() # get all the text in a td
-        scores =response.xpath('//div[@class="cvssbox"]/text()').re(r"\d\.\d") #get all scores in div class cvssbox
-        scores = [i if i != "0.0" else "10.0" for i in scores] # replace 0.0 with 10.0
-        for i in range(len(cves)-2): # 0 to scores length-1
-            self.result.append({"cve": cves[i], 
-             "url": "https://www.cve.org/CVERecord?id="+cves[i], #url to cve
-             "score" : scores[i] #score of cve
-            })
-            print(self.result[i], end="\n\n") #print result)
-        json.dump(self.result, open("result.json", "w")) #save result to json file
+        self.cves= response.xpath('string(//body)').re(r"CVE-\d{4}-\d{4,7}") #get all CVEs regex CVE-dddd-(dddd ddd) <--- 4-7 digits
+        print(self.cves)
+        self.cves = list(set(self.cves)) #remove duplicates
+        self.cves.remove("CVE-2009-1234") #remove this cve because it is not related to vxworks
+        for index, cve in enumerate(self.cves):
+            yield scrapy.Request(
+            url="https://www.cvedetails.com/cve-details.php?cve_id="+str(cve),
+            callback=self.second_parse,
+            meta={"cve": cve, "index": index},
+            dont_filter=True,
+            )
+        print(self.cves,self.scores)
+        
 
+    def second_parse(self, response):
+        temp = response.xpath('//body//*//td//div//text()').getall()  # get all the text in a td 
+        for item in temp :
+            if re.findall("^\d*\.?\d+$",item)!=[]:
+                temp = item
+                break
+        self.scores.append(temp)
+        self.final.append({"cve": response.meta["cve"],
+        "url": "https://www.cve.org/CVERecord?id="+response.meta["cve"], #url to cve
+        "score" : temp #score of cve
+        })
+        print(response.meta["cve"],temp)
+    
+    def spider_closed(self, spider):
+        print("Spider closed")
+        self.scores = [i if i != "0.0" else "10.0" for i in self.scores]
+        json.dump(self.final, open("result.json", "w")) #save result to json file
--- display.py
+++ display.py
@@ -4,6 +4,7 @@ app = flask.Flask(__name__)
 
 f = open("result.json", "r") #open json file with result from Cve.py
 f2 = open("resultAll.json", "r") #open json file with result from CveAll.py
+
 #transform json to html
 def json2html(json):
     table =f'<table border="3"><tr>'
@@ -18,13 +19,15 @@ def json2html(json):
     table += '</table>'
     return table
 
+result1 = json2html(json.load(f))
+result2 = json2html(json.load(f2))
 @app.route('/all')
-def All():
-    return json2html(json.load(f2))
+def All(): 
+    return result2
 
 @app.route('/')
 def index():
-    return json2html(json.loads(f.read()))
+    return result1
 
 if __name__ == '__main__':
     app.run(debug=False,port=80,host='0.0.0.0') #run app
